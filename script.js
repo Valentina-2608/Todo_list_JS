ff1c409e6c71d97fa118d95328d2f409127fdf12
Valetina-2608 <valusha2608@gmail.com>
2022-06-11 17:16:12 +0300

    Add script.js, create function add_todo

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 /* Script */
+
+
+let add_btn=document.getElementById('add_btn');
+let todo_text=document.getElementById('todo_text');
+let parent=document.getElementById('todos');
+add_btn.addEventListener('click', create_todo);
+function create_todo(){
+	let new_todo=document.createElement('div');
+	new_todo.innerHTML=todo_text.value;
+	parent.appendChild(new_todo);
+};
+
