28534a5016622dcb9d77a79feb16ab0692432c65
Valetina-2608 <valusha2608@gmail.com>
2022-06-11 17:19:11 +0300

    Add comments, add function clear_todo

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 
 
 let add_btn=document.getElementById('add_btn');
+let clear_btn=document.getElementById('clear_btn');
+
 let todo_text=document.getElementById('todo_text');
 let parent=document.getElementById('todos');
+
+/* Add new todo*/
 add_btn.addEventListener('click', create_todo);
 function create_todo(){
 	let new_todo=document.createElement('div');
@@ -11,3 +15,9 @@ function create_todo(){
 	parent.appendChild(new_todo);
 };
 
+
+/* Clear todo */
+clear_btn.addEventListener('click', clear_todo);
+function clear_todo(){
+	todo_text.value="";
+}
