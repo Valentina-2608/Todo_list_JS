a5bda84a125c13771bd60cf50a4ea907f471fbe0
Valetina-2608 <valusha2608@gmail.com>
2022-06-12 22:22:59 +0300

    Add check button

--- script.js
+++ script.js
@@ -11,18 +11,33 @@ let parent=document.getElementById('todos');
 add_btn.addEventListener('click', create_todo);
 function create_todo(){
 	let new_todo=document.createElement('div');
-	new_todo.innerHTML=todo_text.value;
 	new_todo.classList.add('new_todo');
 	parent.appendChild(new_todo);
 	
+	
+	let my_todo=document.createElement('div');
+	my_todo.innerHTML=todo_text.value;
+	my_todo.classList.add('my_todo');
+	new_todo.appendChild(my_todo);
+	
+	
+	
 	let remove = document.createElement('button');
 	remove.classList.add('remove_btn');
-	remove.innerHTML = 'delete';
+	remove.innerHTML='Delete todo';
 	new_todo.appendChild(remove);
 	
 	remove.addEventListener('click', function(event) {
 		new_todo.parentElement.removeChild(new_todo);
 		event.preventDefault();
+	});
+	
+	let check = document.createElement('button');
+	check.innerHTML='Check todo';
+	check.classList.add('check_btn');
+	new_todo.appendChild(check);
+	check.addEventListener('click',function(event){
+		my_todo.classList.toggle('check_todo');
 	});
 };
 
